fae205baf7a0650b42029bdc78b555c8d801e64a
Hu Mingzhe <hmz_superb@qq.com>
2022-05-02 18:03:07 -0400

    minor update

--- dqn.py
+++ dqn.py
@@ -162,10 +162,14 @@ def heuristic_agent():
     return
 
 
-def trainer(gamma=0.9,
+def invertible_value_rescale(Q):
+    return tf.math.sign(Q) * (tf.math.sqrt(tf.math.abs(Q) + 1) - 1) + 0.01 * Q
+
+
+def trainer(gamma=0.99,
             batch_size=4,
             learning_rate=0.001,
-            max_memory=3600,
+            max_memory=7200,
             target_update_every=100,
             max_steps_per_episode=3600,
             max_episodes=1000,
@@ -179,9 +183,9 @@ def trainer(gamma=0.9,
     global action_history, state_history, state_next_history, rewards_history, done_history
     # Model used for selecting actions (principal)
     if dueling_dqn:
-        model = ResDQN(is_dueling=True, is_rnn=True)
+        model = Duel_DQN(is_rnn=True)
         # Then create the target model. This will periodically be copied from the principal network
-        model_target = ResDQN(is_dueling=True, is_rnn=True)
+        model_target = Duel_DQN(is_rnn=True)
     else:
         model = DQN(is_rnn=True)
         model_target = DQN(is_rnn=True)
@@ -291,6 +295,7 @@ def trainer(gamma=0.9,
                     Q_next_target = torch_gather(model_target.predict(state_next_sample, batch_size),
                                                  tf.expand_dims(max_Q_index, axis=1), 1)
                     Q_targets = rewards_sample + gamma * tf.squeeze(Q_next_target)
+                Q_targets = invertible_value_rescale(Q_targets)
                 # If the episode was ended (done_sample value is 1)
                 # you can penalize the Q value of the target by some value `penalty`
 
@@ -309,9 +314,9 @@ def trainer(gamma=0.9,
                     loss = loss_function(tf.expand_dims(Q_targets, 1), tf.expand_dims(Q_of_actions, 1))
                     pb.update_priorities(indices, loss.numpy() + 1e-5)
                     try:
-                        loss = loss.mean()
+                        loss = 0.1 * loss.mean() + 0.9 * loss.max()
                     except:
-                        loss = tf.math.reduce_mean(loss)
+                        loss = 0.1 * tf.math.reduce_mean(loss) + 0.9 * tf.math.reduce_max(loss)
 
                 # Nudge the weights of the trainable variables towards
                 grads = tape.gradient(loss, model.trainable_variables)
